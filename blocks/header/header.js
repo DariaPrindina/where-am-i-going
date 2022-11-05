ebef857b17e437df7b69b70b2ece7c3ba387903d
Alisa Krasnopolina <alisakras9@gmail.com>
2022-11-05 14:27:26 +0300

    font changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,7 +1,7 @@
 var menuBtn = document.querySelector('.header__burger');
 var menu = document.querySelector('.header__menu');
 var menuSpan = document.querySelector('.header__span');
-var body = document.querySelector('.root');
+var body = document.querySelector('.page');
 if(menuBtn){
   menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
